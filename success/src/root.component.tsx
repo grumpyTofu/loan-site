fba4efcb2db04cd54d7a47aa1d5b51ed83cbf494
grumpytofu <>
2022-04-23 23:32:58 -0400

    build out success page mfe

--- success/src/root.component.tsx
+++ success/src/root.component.tsx
@@ -1,3 +1,42 @@
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+import React, { useState, useEffect } from "react";
+import { Container, Typography } from "@mui/material";
+
+// How do I share this across MFEs
+interface Applicant {
+  firstName?: string | null;
+  lastName?: string | null;
+  email?: string | null;
+  loanAmount?: number | null;
+  note?: string | null;
 }
+
+const useApplicantInfo = () => {
+  const [applicant, setApplicant] = useState<Applicant>({ firstName: null });
+
+  useEffect(() => {
+    const params = window.location.search
+      .replace("?", "")
+      .split("&")
+      .reduce<Applicant>((applicant, param) => {
+        const [key, value] = param.split("=");
+        if (key === "firstName" && value && value.trim().length > 0) {
+          return { ...applicant, firstName: value };
+        }
+        return applicant;
+      }, {} as Applicant);
+  }, [window.location.search]);
+
+  return { applicant };
+};
+
+const Root: React.FC = () => {
+  const { applicant } = useApplicantInfo();
+
+  return (
+    <Container>
+      <Typography>
+        Congrats{applicant && applicant.firstName ? ` ${applicant.firstName}` : ''}, you submitted the form successfully
+      </Typography>
+    </Container>
+  );
+};
